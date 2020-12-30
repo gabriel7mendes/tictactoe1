d32fca06f34beec199cf8ca0f044a5e7d180abf4
gabriel7mendes <andrewsseven@gmail.com>
2020-12-30 16:53:06 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Game from './components/Game'
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+reportWebVitals();
